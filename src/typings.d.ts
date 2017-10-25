bc98f7632c18cee09b6c4157d633e395212c6f45
Connor Wyatt <connorwyatt1@gmail.com>
2017-10-25 02:45:57 +0100

    add typings for color library

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,14 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+declare var ntc: NameThatColor;
+interface NameThatColor {
+
+  names: Array<Array<string>>;
+  init(): void;
+  name(color: string): Array<string>;
+  hsl(color: string): Array<number>;
+  rgb(color: string): Array<number>;
+
+}
