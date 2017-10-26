b43a7cff67609e1f7b9c0f1067f9954f72fbb486
Connor Wyatt <connorwyatt1@gmail.com>
2017-10-27 00:02:02 +0100

    feat: add color library (#2)
    * add library script file to repo
    
    * add typings for color library
    
    * provide color library to the application and update typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,24 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
-interface NodeModule {
+
+interface NodeModule
+{
   id: string;
+}
+
+declare var ntc: NameThatColor;
+
+interface NameThatColor
+{
+
+  names: Array<{ 0: string, 1: string }>;
+
+  init(): void;
+
+  name(color: string): { 0: string, 1: string };
+
+  hsl(color: string): { 0: number, 1: number, 2: number };
+
+  rgb(color: string): { 0: number, 1: number, 2: number };
+
 }
