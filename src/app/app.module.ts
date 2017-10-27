f09804452bed1be3c55e16507544deaa1e65f5c6
Connor Wyatt <connorwyatt1@gmail.com>
2017-10-27 17:37:31 +0100

    feat: add random color behaviour (#4)
    * add random item iterator
    
    * allow generator to repeat
    
    * add color service for generating colors from the name that color library
    
    * reformat according to prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ColorService } from './color/color.service';
 import { NTC_TOKEN } from './color/ntc.token';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [{ provide: NTC_TOKEN, useValue: ntc }],
+  providers: [ColorService, { provide: NTC_TOKEN, useValue: ntc }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
