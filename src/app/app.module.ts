40ccd069f1372ff3bb355297c5c5c495c271b594
Connor Wyatt <connorwyatt1@gmail.com>
2017-10-27 01:05:15 +0100

    format all files according to prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,9 @@ import { AppComponent } from './app.component';
 import { NTC_TOKEN } from './color/ntc.token';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    { provide: NTC_TOKEN, useValue: ntc }
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: NTC_TOKEN, useValue: ntc }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
