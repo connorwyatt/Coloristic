9c36f94304c7a989d245be6a7fe2f7a75234836f
Connor Wyatt <connorwyatt1@gmail.com>
2017-10-28 18:04:15 +0100

    improve testing by only testing units

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { GameContainerComponent } from './components/game-container/game-container.component';
-import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [AppComponent, GameContainerComponent, HeaderComponent]
+        declarations: [AppComponent],
+        schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
     })
   );
