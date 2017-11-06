5af2d87cc974fe70f415d7e487cfa0d2b3a37e22
Connor Wyatt <connorwyatt1@gmail.com>
2017-11-06 00:24:47 +0000

    update UI to enable gameplay

--- src/app/components/game-container/game-container.component.ts
+++ src/app/components/game-container/game-container.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Color from 'color';
+import { Game } from '../../game';
 import { GameService } from '../../game.service';
 
 @Component({
@@ -7,9 +9,27 @@ import { GameService } from '../../game.service';
   styleUrls: ['./game-container.component.scss']
 })
 export class GameContainerComponent {
+  public currentGame: Game;
+
   private gameService: GameService;
 
   public constructor(gameService: GameService) {
     this.gameService = gameService;
+
+    this.newGame();
+  }
+
+  public submit($event: Event, hex: string) {
+    $event.preventDefault();
+
+    this.guess(hex);
+  }
+
+  public newGame() {
+    this.currentGame = this.gameService.newGame();
+  }
+
+  private guess(hex: string) {
+    console.log(this.currentGame.guess(new Color(hex)));
   }
 }
